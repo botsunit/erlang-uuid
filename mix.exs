9646ca875d8af3290e2ae01ebebeefefdc47936e
glejeune <gregoire.lejeune@free.fr>
2016-11-28 15:44:12 +0100

    Bump to 0.7.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uuid.Mixfile do
   def project do
     [
       app: :uuid,
-      version: "0.7.0",
+      version: "0.7.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
