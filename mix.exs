3085afe78bfe4dba2be4a3e22a6bf8f3a9960523
glejeune <gregoire.lejeune@free.fr>
2016-11-28 15:43:08 +0100

    Prepare 0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uuid.Mixfile do
   def project do
     [
       app: :uuid,
-      version: "0.6.1",
+      version: "0.7.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -26,15 +26,21 @@ defmodule Uuid.Mixfile do
   end
 
   defp aliases do
-    [compile: [&pre_compile_hooks/1, "compile", &post_compile_hooks/1]]
+    [compile: &compile_with_hooks/1]
   end
 
-  defp pre_compile_hooks(_) do
+  defp compile_with_hooks(args) do
+    pre_compile_hooks()
+    :ok = Mix.Task.run("compile", args)
+    post_compile_hooks()
+  end
+
+  defp pre_compile_hooks() do
     run_hook_cmd [
     ]
   end
 
-  defp post_compile_hooks(_) do
+  defp post_compile_hooks() do
     run_hook_cmd [
     ]
   end
